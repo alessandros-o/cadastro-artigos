05d020d2175e926f2d28a02ac71b3434bc1936fc
Alessandro <olive.alessandros@gmail.com>
2022-01-19 00:14:32 -0300

    Configurando Controllers

--- cadastro-artigos/src/main/java/com/example/cadastroartigos/controllers/CategoriaController.java
+++ cadastro-artigos/src/main/java/com/example/cadastroartigos/controllers/CategoriaController.java
@@ -1,5 +1,39 @@
 package com.example.cadastroartigos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cadastroartigos.models.entities.Categoria;
+import com.example.cadastroartigos.models.repository.CategoriaRepositorio;
+
+@RestController
+@RequestMapping("/api/categoria")
 public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepositorio categoriaRepositorio;
+	
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Categoria salvarCategoria(@Validated Categoria categoria) {
+		categoriaRepositorio.save(categoria);
+		return categoria;
+	}
+	
+	@GetMapping
+	public Iterable<Categoria> obterCategoria(){
+		return categoriaRepositorio.findAll();
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void excluirCategoria(@PathVariable int id) {
+		categoriaRepositorio.deleteById(id);
+	}
 
 }
--- cadastro-artigos/src/main/java/com/example/cadastroartigos/controllers/UsuarioController.java
+++ cadastro-artigos/src/main/java/com/example/cadastroartigos/controllers/UsuarioController.java
@@ -1,5 +1,39 @@
 package com.example.cadastroartigos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cadastroartigos.models.entities.Usuario;
+import com.example.cadastroartigos.models.repository.UsuarioRepositorio;
+
+@RestController
+@RequestMapping("/api/usuario")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepositorio usuarioRepositorio;
+	
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Usuario salvarUsuario(@Validated Usuario usuario) {
+		usuarioRepositorio.save(usuario);
+		return usuario;
+	}
+	
+	@GetMapping
+	public Iterable<Usuario> obterUsuario(){
+		return usuarioRepositorio.findAll();
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void excluirUsuario(@PathVariable int id) {
+		usuarioRepositorio.deleteById(id);
+	}
 
 }
